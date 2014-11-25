d99ec14b6a0e7deca73ccd1e906b01bab47cd608
Balloon <Podginator@gmail.com>
2014-11-25 12:35:03 +0000

    changes

--- OGLWin32/OGLScatterplot.cpp
+++ OGLWin32/OGLScatterplot.cpp
@@ -9,6 +9,9 @@ void OGLScatterPlot2D::AddDataSource(DataColumn col)
 
 OGLScatterPlot2D::OGLScatterPlot2D()
 {
+    textSize = 1;
+    text = new OGLText[1];
+    text[0] = OGLText(Vec2f(-300, 0), Color(0, 0, 0), "Scatterplot, add 2 Data Columns (Numerical) to start", "arial.glf", 18);
 }
 
 void OGLScatterPlot2D::InitElements()
@@ -20,7 +23,7 @@ void OGLScatterPlot2D::InitElements()
     dataDist[new OGLLine(Vec2f(-300, -150), Color(0.0, 0.0, 0.0), Vec2f(-300, 200))] = nullptr;
     dataDist[new OGLLine(Vec2f(-300, -150), Color(0.0, 0.0, 0.0), Vec2f(300, -150))] = nullptr;
 
-    //Get BarWidth
+
     Scale(scale);
     Move(-_relativePos.X(), -_relativePos.Y());
     printf("Done");
